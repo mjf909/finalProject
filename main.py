137a2321ff91222852f56bc02715c9875c724ade
Michael Flavin <mjflavin@mail.fhsu.edu>
2022-12-15 11:43:58 -0600

    Changed required input source. Added 3 functions for each time period.

--- main.py
+++ main.py
@@ -3,47 +3,173 @@ import requests
 import json
 
 
+def november_time():
+    request = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate=2022-11-01T22:59:59.999&pubEndDate=2022-11-02T23:59:59.999')
+
+    x = json.loads(request.text)
+
+    for y in range(5):
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+        st.subheader("CVE ID: " + x["vulnerabilities"][0]["cve"]["id"])
+        st.subheader("Source Identifier: " + x["vulnerabilities"][0]["cve"]["sourceIdentifier"])
+        st.subheader("Published Date: " + x["vulnerabilities"][0]["cve"]["published"])
+        st.subheader("Last Modified Date: " + x["vulnerabilities"][0]["cve"]["lastModified"])
+        st.subheader("Vulnerability Status: " + x["vulnerabilities"][0]["cve"]["vulnStatus"])
+        st.subheader("Vulnerability Description: " + x["vulnerabilities"][0]["cve"]["descriptions"][0]["value"])
+        st.subheader(
+            "CVSS Version: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["version"])
+        st.subheader("Attack Vector: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackVector"])
+        st.subheader("Attack Complexity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackComplexity"])
+        st.subheader(
+            "Privileges Required: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "privilegesRequired"])
+        st.subheader("User Interaction Requirement: " +
+                     x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["userInteraction"])
+        st.subheader(
+            "Impact of Confidentiality: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "confidentialityImpact"])
+        st.subheader(
+            "Impact of Integrity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "integrityImpact"])
+        st.subheader(
+            "Impact of Availability: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "availabilityImpact"])
+        st.subheader("Vulnerability Base Score: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseScore"]))
+        st.subheader("Vulnerability Base Severity: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseSeverity"]))
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+
+        y += 1
+
+
+def september_time():
+    request = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate=2022-09-01T22:59:59.999&pubEndDate=2022-09-02T23:59:59.999')
+
+    x = json.loads(request.text)
+
+    for y in range(5):
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+        st.subheader("CVE ID: " + x["vulnerabilities"][0]["cve"]["id"])
+        st.subheader("Source Identifier: " + x["vulnerabilities"][0]["cve"]["sourceIdentifier"])
+        st.subheader("Published Date: " + x["vulnerabilities"][0]["cve"]["published"])
+        st.subheader("Last Modified Date: " + x["vulnerabilities"][0]["cve"]["lastModified"])
+        st.subheader("Vulnerability Status: " + x["vulnerabilities"][0]["cve"]["vulnStatus"])
+        st.subheader("Vulnerability Description: " + x["vulnerabilities"][0]["cve"]["descriptions"][0]["value"])
+        st.subheader(
+            "CVSS Version: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["version"])
+        st.subheader("Attack Vector: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackVector"])
+        st.subheader("Attack Complexity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackComplexity"])
+        st.subheader(
+            "Privileges Required: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "privilegesRequired"])
+        st.subheader("User Interaction Requirement: " +
+                     x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["userInteraction"])
+        st.subheader(
+            "Impact of Confidentiality: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "confidentialityImpact"])
+        st.subheader(
+            "Impact of Integrity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "integrityImpact"])
+        st.subheader(
+            "Impact of Availability: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "availabilityImpact"])
+        st.subheader("Vulnerability Base Score: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseScore"]))
+        st.subheader("Vulnerability Base Severity: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseSeverity"]))
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+
+        y += 1
+
+
+
+def august_time():
+    request = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate=2022-07-01T22:59:59.999&pubEndDate=2022-07-02T23:59:59.999')
+
+    x = json.loads(request.text)
+
+    for y in range(5):
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+        st.subheader("CVE ID: " + x["vulnerabilities"][0]["cve"]["id"])
+        st.subheader("Source Identifier: " + x["vulnerabilities"][0]["cve"]["sourceIdentifier"])
+        st.subheader("Published Date: " + x["vulnerabilities"][0]["cve"]["published"])
+        st.subheader("Last Modified Date: " + x["vulnerabilities"][0]["cve"]["lastModified"])
+        st.subheader("Vulnerability Status: " + x["vulnerabilities"][0]["cve"]["vulnStatus"])
+        st.subheader("Vulnerability Description: " + x["vulnerabilities"][0]["cve"]["descriptions"][0]["value"])
+        st.subheader(
+            "CVSS Version: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["version"])
+        st.subheader("Attack Vector: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackVector"])
+        st.subheader("Attack Complexity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+            "attackComplexity"])
+        st.subheader(
+            "Privileges Required: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "privilegesRequired"])
+        st.subheader("User Interaction Requirement: " +
+                     x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["userInteraction"])
+        st.subheader(
+            "Impact of Confidentiality: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "confidentialityImpact"])
+        st.subheader(
+            "Impact of Integrity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "integrityImpact"])
+        st.subheader(
+            "Impact of Availability: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"][
+                "availabilityImpact"])
+        st.subheader("Vulnerability Base Score: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseScore"]))
+        st.subheader("Vulnerability Base Severity: " + str(
+            x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseSeverity"]))
+        st.subheader(
+            "----------------------------------------------------------------------------------------------------")
+
+        y += 1
 
 st.title("Vulnerability Lookup")
 
 
-st.subheader("USAGE: Simply enter a keyword such as \"Microsoft\" or \"Debian\" to view vulnerabilities regarding your search. ")
+st.subheader("USAGE: Enter a 1, 2, or 3 to view published NVD vulnerabilities during the selected time period.")
+
+st.subheader("1 = Between November 1st and November 2nd 2022")
+st.subheader("2 = Between September 1st and September 2nd 2022")
+st.subheader("3 = Between August 1st and August 2nd 2022")
 
 
 
-name = st.text_input("Enter a keyword", "")
+time = st.text_input("Enter a 1, 2, or 3", "")
 
-if(st.button('Submit')):
-    result = name.title()
+
+
+
+if (st.button('Submit')):
+    result = time.title()
     st.success(result)
 
+    if (time == "1"):
+        november_time()
+
+    elif (time == "2"):
+        september_time()
+
+    elif (time == "3"):
+        august_time()
+
+    else:
+        print("Please enter a 1, 2, or 3.")
 
 
-request = requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate=2022-11-01T22:59:59.999&pubEndDate=2022-11-01T23:59:59.999')
 
 
-# parse x:
-x = json.loads(request.text)
 
-st.subheader("-----------------------------------------------------")
-
-st.subheader("CVE ID: " + x["vulnerabilities"][0]["cve"]["id"])
-st.subheader("Source Identifier: " + x["vulnerabilities"][0]["cve"]["sourceIdentifier"])
-st.subheader("Published Date: " + x["vulnerabilities"][0]["cve"]["published"])
-st.subheader("Last Modified Date: " + x["vulnerabilities"][0]["cve"]["lastModified"])
-st.subheader("Vulnerability Status: " + x["vulnerabilities"][0]["cve"]["vulnStatus"])
-st.subheader("Vulnerability Description: " + x["vulnerabilities"][0]["cve"]["descriptions"][0]["value"])
-st.subheader("CVSS Version: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["version"])
-st.subheader("Attack Vector: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["attackVector"])
-st.subheader("Attack Complexity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["attackComplexity"])
-st.subheader("Privileges Required: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["privilegesRequired"])
-st.subheader("User Interaction Requirement: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["userInteraction"])
-st.subheader("Impact of Confidentiality: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["confidentialityImpact"])
-st.subheader("Impact of Integrity: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["integrityImpact"])
-st.subheader("Impact of Availability: " + x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["availabilityImpact"])
-st.subheader("Vulnerability Base Score: " + str(x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseScore"]))
-st.subheader("Vulnerability Base Severity: " + str(x["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0]["cvssData"]["baseSeverity"]))
-
-st.subheader("-----------------------------------------------------")
 
 
